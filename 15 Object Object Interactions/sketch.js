a0c18cc46dec7313d2a9954c8be6991b1b0203ac
Montierleo <montierleo1@gmail.com>
2026-03-25 12:18:38 -0600

    15

---
+++ 15 Object Object Interactions/sketch.js
@@ -0,0 +1,84 @@
+// Object Object Interactions
+// Montier Liu
+// Mar 25
+
+let nodes = [];
+let reach = 150;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0,20);
+  //if no deletions, loop by item
+  for(let n of nodes){
+    n.move();
+    n.connect(nodes);
+    n.display();
+  }
+}
+
+function mousePressed(){
+  if(mouseButton === "left"){
+    for(let i = 0; i < 50; i++){
+      let x = random(-50, 50);
+      let y = random(-50, 50);
+      nodes.push(new csNode(mouseX+x, mouseY+y));
+    }
+  }
+}
+
+class csNode{
+  //1. constructor
+  constructor(x,y){
+    //properties related to pos/display
+    this.x = x; this.y = y; this.size = 10;
+    this.c = color(random(255), random(255), random(255));
+
+    //properties related to movement
+    this.xTime = random(100); this.yTime = random(100);
+    this.timeShift = 0.01; this.maxSpeed = 5;
+  }
+
+  //2. Class Methods
+  display(){
+    fill(this.c);
+    noStroke();
+    circle(this.x, this.y, this.size);
+  }
+
+  move(){
+    //use perlin noise for x,y movement
+    let xSpeed = noise(this.xTime);  //0-1
+    xSpeed = map(xSpeed, 0, 1, -this.maxSpeed, this.maxSpeed);
+    this.xTime += this.timeShift;
+
+    this.x += xSpeed;
+    if(this.x < 0) this.x = width;
+    else if (this.x > width) this.x = 0;
+
+    //Do the same thing for y component
+    let ySpeed = noise(this.yTime);  //0-1
+    ySpeed = map(ySpeed, 0, 1, -this.maxSpeed, this.maxSpeed);
+    this.yTime += this.timeShift;
+
+    this.y += ySpeed;
+    if(this.y < 0) this.y = height;
+    else if(this.y > height) this.y = 0;
+  }
+
+  connect(nodeArray){
+    //check if the current csNode is close to any other
+    //csNode, and if so join with a line.
+    stroke(this.c);
+    for(let n of nodeArray){
+      if(n !== this){
+        let d = dist(this.x,this.y,n.x,n.y);
+        if(d < reach){
+          line(this.x, this.y, n.x, n.y);
+        }
+      }
+    }
+  }
+}
