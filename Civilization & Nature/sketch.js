ab3ed070eb12ef88e005ab8a664ba20701c517fc
Montierleo <montierleo1@gmail.com>
2026-05-12 12:54:53 -0600

    "Nature" Project
    basic structure built

--- Civilization & Nature/sketch.js
+++ Civilization & Nature/sketch.js
@@ -6,6 +6,12 @@
 let canvasW = 900;
 let canvasH = 600;
 
+// Split the text into characters
+let chars = [];
+
+// Dots
+let dots = [];
+
 // Text
 let message = "Hello world";
 let fontSizeValue = 32;
@@ -15,6 +21,8 @@ let textY = 100;
 
 function setup() {
   createCanvas (canvasW, canvasH);
+
+  splitText();
 }
 
 function draw() {
@@ -23,5 +31,75 @@ function draw() {
   fill(textColor);
   textSize(fontSizeValue);
 
-  text(message, textX, textY)
+  for(let c of chars){
+    c.x = lerp(c.x, c.targetX, 0.05);
+    c.y = lerp(c.y, c.targetY, 0.05);
+    text(c.char, c.x, c.y);
+  }
+
+  for(let d of dots){
+    stroke(30);
+    strokeWeight(2);
+
+    line(
+     d.x,
+     d.y,
+     d.x,
+     d.y + d.lineLength
+    );
+    
+    noStroke;
+    fill("red");
+
+    ellipse(d.x, d.y, 20);
+  }
+
+  if(frameCount % 60 == 0){
+    dots.push({
+      x: random(100, 800),
+      y: random(100, 500),
+      lineLength: random(50, 100)
+    }); 
+
+    let randomChar = random(chars);
+    randomChar.targetX = random(100, 800);
+    randomChar.targetY = random(100, 500);
+    
+  }
+  
+}
+
+function splitText(){
+  textSize(fontSizeValue);
+
+  let marginLeft = 80;
+  let marginRight = 80;
+
+  let x = marginLeft;
+  let y = 100;
+
+  let maxWidth = width - marginLeft - marginRight;
+
+  
+  for(let i = 0; i < message.length; i++){
+    let ch = message[i];
+    let w = textWidth(ch);
+    
+
+    // Automatic line change
+    if(x + w > marginLeft + maxWidth){
+      x = marginLeft
+      y += 50;
+    }
+
+    chars.push({
+      char: ch,
+      x: x,
+      y: y,
+      targetX: x,
+      targetY: y
+    });
+
+    x += w + 2;
+  }
 }
